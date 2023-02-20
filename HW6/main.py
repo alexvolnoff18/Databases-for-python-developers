f034d09fde99c017d51edef7202a5ed528a64117
alexvolnoff18 <113288005+alexvolnoff18@users.noreply.github.com>
2023-02-20 10:46:28 +0300

    Update main.py

--- HW6/main.py
+++ HW6/main.py
@@ -33,5 +33,27 @@ for line in data:
         'sale': Sale,
     }[line['model']]
     session.add(method(id=line['pk'], **line.get('fields')))
+    
+def searching_publisher_name():
+    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
+    query_publisher_name = input('Введите имя (name) издателя: ')
+    query_result = query_join.filter(Publisher.publisher_name == query_publisher_name)
+    for result in query_result.all():
+        print(f'Издатель "{query_publisher_name}" найден в магазине "{result.name}" с идентификатором {result.id}')
+
+
+def searching_publisher_id():
+    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
+    query_publisher_name = input('Введите идентификатор (id) издателя: ')
+    query_result = query_join.filter(Publisher.id_publisher == query_publisher_name)
+    for result in query_result.all():
+        print(
+            f'Издатель c id: {query_publisher_name} найден в магазине "{result.name}" '
+            f'с идентификатором {result.id}')
+
+
+if __name__ == '__main__':
+    searching_publisher_name()
+    searching_publisher_id()
 
 session.commit()
