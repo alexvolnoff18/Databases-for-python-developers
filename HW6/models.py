1789a25c74c3d5ae4c3bbef46a070e6d67e07acc
alexvolnoff18 <113288005+alexvolnoff18@users.noreply.github.com>
2023-06-27 10:33:09 +0300

    Update models.py

--- HW6/models.py
+++ HW6/models.py
@@ -1,55 +1,81 @@
-
-
-import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship
+import sqlalchemy
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 Base = declarative_base()
 
 
+DSN = "postgresql://postgres:wsxzaq!1@localhost:5432/HomeWork6"
+engine = sqlalchemy.create_engine(DSN)
+
 class Publisher(Base):
-    __tablename__ =  'publishers'
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String, unique=True)
-    def __repr__(self):
-        return f"Publisher(id={self.id}, name={self.name})"
+    __tablename__ = "publisher"
+
+    id_publisher = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    publisher_name = sqlalchemy.Column(sqlalchemy.String(length=100), nullable=False, unique=True)
+
+    def __str__(self):
+        return f'Publisher {self.id} : {self.name}'
+
 
 
 class Book(Base):
-    __tablename__ =  'books'
-    id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.String)
-    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publishers.id"), nullable=False)
-    publisher = relationship("Publisher", backref="books")
+    __tablename__ = "book"
+
+    id_book = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    book_title = sqlalchemy.Column(sqlalchemy.String(length=100), nullable=False)
+    id_publisher = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("publisher.id_publisher"), nullable=False)
+    publisher = relationship(Publisher, backref="book")
+
+    def __str__(self):
+        return f'Book {self.id_book} : ({self.book_title}, {self.id_publisher})'
 
 
 
 class Shop(Base):
-    __tablename__ =  'shops'
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String, unique=True)
-    def __repr__(self):
-        return f"Shop(id={self.id}, name={self.name})"
+    __tablename__ = "shop"
+
+    id_shop = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    shop_name = sqlalchemy.Column(sqlalchemy.String(length=100), nullable=False)
+
+    def __str__(self):
+        return f'Shop {self.id_shop} : ({self.shop_name})'
+
 
 
 class Stock(Base):
-    __tablename__ = 'stocks'
-    id = sq.Column(sq.Integer, primary_key=True)
-    id_book = sq.Column(sq.Integer, sq.ForeignKey("books.id"), nullable=False)
-    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shops.id"), nullable=False)
-    count = sq.Column(sq.Integer, nullable=False)
-    book = relationship("Book", backref="stocks")
-    shop = relationship("Shop", backref="stocks")
-    sales = relationship("Sale", backref="stock")
+    __tablename__ = "stock"
+
+    id_stock = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    stock_count = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    id_book = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("book.id_book"), nullable=False)
+    id_shop = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("shop.id_shop"), nullable=False)
+    book = relationship(Book, backref="stock")
+    shop = relationship(Shop, backref="stock")
+
+    def __str__(self):
+        return f'Stock {self.id_stock} : ({self.stock_count}, {self.id_book}, {self.id_shop})'
+
 
 
 class Sale(Base):
-    __tablename__ = 'sales'
-    id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Float, nullable=False)
-    date_sale = sq.Column(sq.Date)
-    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stocks.id"), nullable=False)
-    count = sq.Column(sq.Integer, nullable=False)
+    __tablename__ = "sale"
+
+    id_sale = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    sale_price = sqlalchemy.Column(sqlalchemy.Numeric, nullable=False)
+    sale_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
+    sale_count = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    id_stock = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("stock.id_stock"), nullable=False)
+    stock = relationship(Stock, backref="sale")
+
+    def __str__(self):
+        return f'Sale {self.id_sale} : ({self.sale_price}, {self.sale_date}, {self.sale_count}, {self.id_stock})'
 
 
-def create_tables(engine):
-    Base.metadata.create_all(engine)
+
+Base.metadata.create_all(engine)
+
+# def create_tables(engine):
+#     Base.metadata.create_all(engine)
+
+# def drop_tables(engine):
+#     Base.metadata.drop_all(engine)
